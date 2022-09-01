50ec2a3abfad186d90486e83937e49138bd01748
威 刘 <lw1140@163.com>
2022-09-01 10:26:53 +0800

    fix: 启用插件时对点击事件进行拦截

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,24 +7,31 @@ export function bindEvent() {
       window.addEventListener('mouseover', evt => {
         (evt.target as Element).classList.add('md-to-html__highline');
       });
-      window.addEventListener('click', evt => {
-        const target = evt.target as Element;
-        if (target) {
-          const selectElements = document.querySelectorAll(
-            '.me-to-html__selected'
-          );
-          if (selectElements.length) {
-            for (let index = 0; index < selectElements.length; index++) {
-              const element = selectElements[index] as Element;
-              element.classList.remove('me-to-html__selected');
+      window.addEventListener(
+        'click',
+        evt => {
+          const target = evt.target as Element;
+          if (target) {
+            const selectElements = document.querySelectorAll(
+              '.md-to-html__selected'
+            );
+            if (selectElements.length) {
+              for (let index = 0; index < selectElements.length; index++) {
+                const element = selectElements[index] as Element;
+                element.classList.remove('md-to-html__selected');
+              }
+            }
+            const hoverActive = target.classList.contains(
+              'md-to-html__highline'
+            );
+            if (hoverActive) {
+              target.classList.add('md-to-html__selected');
+              evt?.preventDefault();
             }
           }
-          const hoverActive = target.classList.contains('md-to-html__highline');
-          if (hoverActive) {
-            target.classList.add('me-to-html__selected');
-          }
-        }
-      });
+        },
+        true
+      );
     });
   }
 }
@@ -37,7 +44,7 @@ export function addStyles() {
       outline-offset: -1px !important;
       box-shadow: rgb(0 0 0 / 80%) 0px 2px 8px !important;
     }
-    .me-to-html__selected {
+    .md-to-html__selected {
       outline: rgb(73, 196, 237) solid 2px !important;
       outline-offset: -1px !important;
       box-shadow: rgb(0 0 0 / 80%) 0px 2px 8px !important;
